8912ab75d9bea03bb482d8249cb42e2289797eff
tejapathuri <tejatheyellowarmy@gmail.com>
2022-11-02 21:05:43 -0500

    changes made

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var DiningRouter = require('./routes/Dining');
+var gridbuildRouter = require('./routes/gridbuild');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -23,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Dining', DiningRouter);
+app.use('/gridbuild', gridbuildRouter);
+app.use('/selector', selectorRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/gridbuild.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    let query = req.query
+    rows=query.rows
+    cols=query.cols
+console.log(`rows ${query.rows}`)
+console.log(`cols ${query.cols}`)
+
+
+  res.render('gridbuild', { title: 'Grid Build',query:query });
+});
+
+module.exports = router;
